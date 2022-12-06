4265bfd425a6eb32613bb075c1902fbff538cb85
Lauren Seward <lauren.unal@gmail.com>
2022-12-06 14:13:25 -0600

    Complete dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,24 @@
+class Dragon {
+    constructor(name, rider) {
+        this.name = name
+        this.rider = rider
+        this.hungry = true
+        this.eats = 0
+        this.greet = function greet() {
+            return `Hi, ${rider}!`
+        }
+    } 
+    eat() {
+        this.eats += + 1;
+     if (this.eats === 3) {
+        this.hungry = false
+     }
+    }
+}
 
+
+
+
+
+
+module.exports = Dragon
