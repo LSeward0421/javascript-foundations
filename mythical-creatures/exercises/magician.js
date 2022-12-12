1e2ff8b407f509337b87ee429c3332106c5258c1
Lauren Seward <lauren.unal@gmail.com>
2022-12-12 17:26:10 -0600

    Complete magician

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -1,15 +1,35 @@
 class Magician {
-    constructor (magician) {
-        this.name = `The Great ${magician.name}`
-        this.assistant = magician.assistant
-        this.accessory = magician.accessory
-        this.favoriteAccessory = magician.accessory || `top hat`
-
+    
+    constructor(magician) {
+        this.name = `The Great ${magician.name}`;
+        this.assistant = magician.assistant;
+        // this.clothing = magician.clothing;
+        this.favoriteAccessory = magician.clothing || 'top hat';
+        this.confidencePercentage = 10;
     }
+
     performIncantation(incantation) {
-        return `${incantation}!`.toUpperCase()
+        return `${incantation}!`.toUpperCase();
+    }
+
+    performTrick() {
+        this.confidencePercentage += 10;
+        if (this.clothing === 'cape') {
+            return 'PULL DOVE FROM SLEEVE';
+        } else {
+            return 'PULL RABBIT FROM TOP HAT';
+        }   
+    }
+
+    performShowStopper() {
+        if (this.confidencePercentage <= 100 || this.assistant === false) {
+            return 'Oh no, this trick is not ready!';
+        } else {
+            return 'WOW! The magician totally just sawed that person in half!';
+        }
     }
 }
+
 
 
 
