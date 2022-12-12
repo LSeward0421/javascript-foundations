5e410b91aadbf162c40e54d4fd2fef9be63e75ae
Lauren Seward <lauren.unal@gmail.com>
2022-12-11 18:45:39 -0600

    Complete 3 tests

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,12 @@
+class Magician {
+    constructor (magician) {
+        this.name = `The Great ${magician.name}`
+        this.assistant = magician.assistant
+
+    }
+}
+
+
+
+
+module.exports = Magician
