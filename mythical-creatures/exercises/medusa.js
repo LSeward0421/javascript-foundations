d6cc169a195f74b242941a988068f97c4592dbf3
Lauren Seward <lauren.unal@gmail.com>
2022-12-15 10:24:23 -0600

    Complete Medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,25 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+
+class Medusa {
+    constructor(name) {
+        this.name = name
+        this.statues = []
+    }
+    gazeAtVictim(person) {
+        var newStatue = new Statue(person.name)
+        if(this.statues.length <= 2) {
+        this.statues.push(newStatue)
+    } else if (this.statues.length === 3) {
+        var freedVictim = new Person(this.statues[0].name)
+        this.statues.shift()
+        this.statues.push(newStatue)
+        freedVictim.mood = 'relieved'
+       return freedVictim
+    }
+    }
+  }
+
+
+module.exports = Medusa
