026800e6b50906a6024fe7e010b5603bb9c2407f
Lauren Seward <lauren.unal@gmail.com>
2023-01-10 13:33:49 -0600

    Complete craft/material

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,20 @@
 class Craft {
+  constructor(hobbyObj) {
+    this.name = hobbyObj.type
+    this.materials = hobbyObj.materials
+    this.completed = false
+ }
+ completeCraft() {
+  this.completed = true;
+  return 'All done! It looks great!'
+ }
+ calculateProjectTotal() {
+  console.log(this.materials[0].calculateMaterialCost())
 
+  var sum = this.materials[0].calculateMaterialCost() + this.materials[1].calculateMaterialCost()
+
+  return sum
+ }
 }
 
 module.exports = Craft;
